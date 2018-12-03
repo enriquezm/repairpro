b06628c11d503dd5dcbc30871706ede5e6aa3800
Myles Enriquez <enri.myles@gmail.com>
2018-12-02 18:26:11 -0800

    Add basic markup structure.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,56 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
+  <section>
+    <h1>RepairProLV</h1>
     <p>Welcome to your new Gatsby site.</p>
+  </section>
+  <section>
+    <h2>Why choose us?</h2>
+    <div>
+      <div>
+        <h3>Experience</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius non neque ut fermentum. Cras eros velit, iaculis non suscipit in, rhoncus sed lacus. Morbi sed blandit felis. Fusce tempor diam in justo fringilla, id condimentum quam porttitor. Proin et est aliquam, commodo quam eget, fringilla mauris. Phasellus laoreet, elit ut porttitor ultricies, ipsum elit venenatis ipsum.
+        </p>
+      </div>
+      <div>
+        <h3>Reputation</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius non neque ut fermentum. Cras eros velit, iaculis non suscipit in, rhoncus sed lacus. Morbi sed blandit felis. Fusce tempor diam in justo fringilla, id condimentum quam porttitor. Proin et est aliquam, commodo quam eget, fringilla mauris. Phasellus laoreet, elit ut porttitor ultricies, ipsum elit venenatis ipsum.
+        </p>
+      </div>
+      <div>
+        <h3>Passion</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce varius non neque ut fermentum. Cras eros velit, iaculis non suscipit in, rhoncus sed lacus. Morbi sed blandit felis. Fusce tempor diam in justo fringilla, id condimentum quam porttitor. Proin et est aliquam, commodo quam eget, fringilla mauris. Phasellus laoreet, elit ut porttitor ultricies, ipsum elit venenatis ipsum.
+        </p>
+      </div>
+    </div>
+  </section>
+  <section>
+    <h2>Our services</h2>
+    <div>
+      <div>
+        <h3>AC Repair</h3>
+      </div>
+      <div>
+        <h3>Tire Rotation</h3>
+      </div>
+      <div>
+        <h3>Oil Change</h3>
+      </div>
+      <div>
+        <h3>Transmission Repair</h3>
+      </div>
+      <div>
+        <h3>Suspension Stuff</h3>
+      </div>
+      <div>
+        <h3>More Car Stuff</h3>
+      </div>
+    </div>
+  </section>
   </Layout>
 )
 
